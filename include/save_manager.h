7e01914d35c5f0456ec7a5125b31b7dcbfb1cf09
Gocario <Gocario.dev@gmail.com>
2016-01-07 02:12:45 +0100

    Add the save manager

---
+++ include/save_manager.h
@@ -0,0 +1,37 @@
+#pragma once
+/**
+ * @file fs.h
+ * @brief Filesystem Services
+ */
+#ifndef SAVE_MANAGER_H
+#define SAVE_MANAGER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <3ds/types.h>
+
+/**
+ * @brief Export the savedata from the card/title to the SD.
+ * @return The error encountered.
+ */
+Result Save_exportSavedata(void);
+
+/**
+ * @brief Import the savedata from the SD to the card/title.
+ * @return The error encountered.
+ */
+Result Save_importSavedata(void);
+
+/**
+ * @brief Backup the savedata from the card/title to the SD.
+ * @return The error encountered.
+ */
+Result Save_backupSavedata(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SAVE_MANAGER_H
---
+++ source/save_manager.c
@@ -0,0 +1,79 @@
+#include "save_manager.h"
+#include "fs.h"
+
+#include <3ds/result.h>
+#include <3ds/os.h>
+
+#include <stdio.h>
+
+#define SAVEDATA_MAX_SIZE 0x76000 // SAVEDATA_ORAS_SIZE
+
+static const char* baseFolder = "/pk/";
+static const char* saveFolder = "/pk/save/";
+static const char* backupFolder = "/pk/backup/";
+
+Result Save_exportSavedata(void)
+{
+	Result ret;
+	char path[32];
+
+	u32 bytesRead, bytesWritten;
+	u8 savedata[SAVEDATA_MAX_SIZE];
+
+	ret = FS_CreateDirectory(baseFolder, &sdmcArchive);
+	ret = FS_CreateDirectory(saveFolder, &sdmcArchive);
+	
+	sprintf(path, "main");
+	ret = FS_ReadFile(path, (void*) savedata, &saveArchive, SAVEDATA_MAX_SIZE, &bytesRead);
+	if (R_FAILED(ret)) return ret;
+
+	sprintf(path, "%smain", saveFolder);
+	ret = FS_DeleteFile(path, &sdmcArchive);
+	ret = FS_WriteFile(path, (void*) savedata, bytesRead, &sdmcArchive, &bytesWritten);
+	if (R_FAILED(ret)) return ret;
+
+	return ret;
+}
+
+
+Result Save_importSavedata(void)
+{
+	Result ret;
+	char path[32];
+
+	u32 bytesRead, bytesWritten;
+	u8 savedata[SAVEDATA_MAX_SIZE];
+
+	sprintf(path, "%smain", saveFolder);
+	ret = FS_ReadFile(path, (void*) savedata, &sdmcArchive, SAVEDATA_MAX_SIZE, &bytesRead);
+	if (R_FAILED(ret)) return ret;
+
+	sprintf(path, "main");
+	ret = FS_DeleteFile(path, &saveArchive);
+	ret = FS_WriteFile(path, (void*) savedata, bytesRead, &saveArchive, &bytesWritten);
+	if (R_FAILED(ret)) return ret;
+
+	return ret;
+}
+
+
+Result Save_backupSavedata(void)
+{
+	Result ret;
+	char path[32];
+
+	u32 bytesRead, bytesWritten;
+	u8 savedata[SAVEDATA_MAX_SIZE];
+
+	ret = FS_CreateDirectory(baseFolder, &sdmcArchive);
+	ret = FS_CreateDirectory(backupFolder, &sdmcArchive);
+
+	sprintf(path, "main");
+	ret = FS_ReadFile(path, (void*) savedata, &sdmcArchive, SAVEDATA_MAX_SIZE, &bytesRead);
+	if (R_FAILED(ret)) return ret;
+
+	sprintf(path, "%smain_%lli", backupFolder, osGetTime() / 1000 - 2208988800L);
+	ret = FS_WriteFile(path, (void*) savedata, bytesRead, &saveArchive, &bytesWritten);
+
+	return ret;
+}
