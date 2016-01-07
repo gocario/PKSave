e65d5d2c33afa043ce189d447b5b4e44992130ab
Gocario <Gocario.dev@gmail.com>
2016-01-07 17:13:35 +0100

    Working save manager
    - Use malloc instead of array for savedata
    - Fix the paths and archive used

--- source/save_manager.c
+++ source/save_manager.c
@@ -5,9 +5,11 @@
 #include <3ds/os.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define SAVEDATA_MAX_SIZE 0x76000 // SAVEDATA_ORAS_SIZE
 
+static const char* rootFolder = "/";
 static const char* baseFolder = "/pk/";
 static const char* saveFolder = "/pk/save/";
 static const char* backupFolder = "/pk/backup/";
@@ -18,20 +20,22 @@ Result Save_exportSavedata(void)
 	char path[32];
 
 	u32 bytesRead, bytesWritten;
-	u8 savedata[SAVEDATA_MAX_SIZE];
+	u8* savedata = malloc(SAVEDATA_MAX_SIZE * sizeof(u8));
 
 	ret = FS_CreateDirectory((char*) baseFolder, &sdmcArchive);
 	ret = FS_CreateDirectory((char*) saveFolder, &sdmcArchive);
 
-	sprintf(path, "main");
-	ret = FS_ReadFile(path, (void*) savedata, &saveArchive, SAVEDATA_MAX_SIZE, &bytesRead);
-	if (R_FAILED(ret)) return ret;
+	sprintf(path, "%smain", rootFolder);
+	ret = FS_ReadFile(path, savedata, &saveArchive, SAVEDATA_MAX_SIZE, &bytesRead);
 
-	sprintf(path, "%smain", (char*) saveFolder);
-	ret = FS_DeleteFile(path, &sdmcArchive);
-	ret = FS_WriteFile(path, (void*) savedata, bytesRead, &sdmcArchive, &bytesWritten);
-	if (R_FAILED(ret)) return ret;
+	if (R_SUCCEEDED(ret))
+	{
+		sprintf(path, "%smain", (char*) saveFolder);
+		ret = FS_DeleteFile(path, &sdmcArchive);
+		ret = FS_WriteFile(path, savedata, bytesRead, &sdmcArchive, &bytesWritten);
+	}
 
+	free(savedata);
 	return ret;
 }
 
@@ -42,17 +46,19 @@ Result Save_importSavedata(void)
 	char path[32];
 
 	u32 bytesRead, bytesWritten;
-	u8 savedata[SAVEDATA_MAX_SIZE];
+	u8* savedata = malloc(SAVEDATA_MAX_SIZE * sizeof(u8));
 
 	sprintf(path, "%smain", (char*) saveFolder);
 	ret = FS_ReadFile(path, (void*) savedata, &sdmcArchive, SAVEDATA_MAX_SIZE, &bytesRead);
-	if (R_FAILED(ret)) return ret;
+	
+	if (R_SUCCEEDED(ret))
+	{
+		sprintf(path, "%smain", rootFolder);
+		ret = FS_DeleteFile(path, &saveArchive);
+		ret = FS_WriteFile(path, (void*) savedata, bytesRead, &saveArchive, &bytesWritten);
+	}
 
-	sprintf(path, "main");
-	ret = FS_DeleteFile(path, &saveArchive);
-	ret = FS_WriteFile(path, (void*) savedata, bytesRead, &saveArchive, &bytesWritten);
-	if (R_FAILED(ret)) return ret;
-
+	free(savedata);
 	return ret;
 }
 
@@ -63,17 +69,21 @@ Result Save_backupSavedata(void)
 	char path[32];
 
 	u32 bytesRead, bytesWritten;
-	u8 savedata[SAVEDATA_MAX_SIZE];
+	u8* savedata = malloc(SAVEDATA_MAX_SIZE * sizeof(u8));
 
 	ret = FS_CreateDirectory((char*) baseFolder, &sdmcArchive);
 	ret = FS_CreateDirectory((char*) backupFolder, &sdmcArchive);
 
-	sprintf(path, "main");
-	ret = FS_ReadFile(path, (void*) savedata, &sdmcArchive, SAVEDATA_MAX_SIZE, &bytesRead);
-	if (R_FAILED(ret)) return ret;
+	sprintf(path, "%smain", rootFolder);
+	ret = FS_ReadFile(path, savedata, &saveArchive, SAVEDATA_MAX_SIZE, &bytesRead);
+	
+	if (R_SUCCEEDED(ret))
+	{
+		sprintf(path, "%smain_%lli", backupFolder, osGetTime()/* - 2208988800L*/);
+		ret = FS_WriteFile(path, savedata, bytesRead, &sdmcArchive, &bytesWritten);
+	}
 
-	sprintf(path, "%smain_%lli", backupFolder, osGetTime() / 1000 - 2208988800L);
-	ret = FS_WriteFile(path, (void*) savedata, bytesRead, &saveArchive, &bytesWritten);
+	free(savedata);
 
 	return ret;
 }
